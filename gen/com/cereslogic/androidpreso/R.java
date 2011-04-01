99d90e5767a9800b0b0b664f88882dee8c692ba3
Mike Desjardins <desjardinsmike@gmail.com>
2011-04-01 11:18:55 -0400

    Hello world project.

--- gen/com/cereslogic/androidpreso/R.java
+++ gen/com/cereslogic/androidpreso/R.java
@@ -18,7 +18,6 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int deliver_bacon=0x7f040002;
         public static final int hello=0x7f040000;
     }
 }
