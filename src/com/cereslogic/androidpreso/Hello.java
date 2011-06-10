5790c2889cc4f71e660f69f2e64ef0862e99acda
Mike Desjardins <mike.desjardins@cereslogic.com>
2011-06-10 11:06:49 -0400

    Put showTreats method into Hello.java.

--- src/com/cereslogic/androidpreso/Hello.java
+++ src/com/cereslogic/androidpreso/Hello.java
@@ -1,6 +1,8 @@
 package com.cereslogic.androidpreso;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,6 +15,12 @@ public class Hello extends Activity {
         setContentView(R.layout.main);
     }
 
+    public void showBaconTreats(View view) {
+      Intent intent = new Intent(Intent.ACTION_VIEW);
+      intent.setComponent(new ComponentName("com.cereslogic.androidpreso","com.cereslogic.androidpreso.BaconTreats"));
+      startActivity(intent);
+    }
+    
     public void deliverBacon(View view) {
     	ImageView i = (ImageView)findViewById(R.id.bacon_image);
     	if (i.getVisibility() == View.INVISIBLE) {
