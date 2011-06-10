ca7b79e7b93f5b110cb3011e176a4db78357bfd3
Mike Desjardins <mike.desjardins@cereslogic.com>
2011-06-10 10:59:47 -0400

    Fixing bugs.

--- gen/com/cereslogic/androidpreso/R.java
+++ gen/com/cereslogic/androidpreso/R.java
@@ -22,7 +22,18 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int bacon_and_eggs=0x7f040007;
+        public static final int bacon_bits=0x7f040009;
+        public static final int bacon_cheeseburger=0x7f040008;
+        public static final int bacon_chewing_gum=0x7f04000b;
+        public static final int bacon_ice_cream=0x7f04000a;
+        public static final int bacon_lettuce_tomato_sandwich=0x7f040006;
+        public static final int bacon_lollipop=0x7f04000c;
+        public static final int bacon_plain=0x7f040005;
+        public static final int bacon_treat_list=0x7f040004;
+        public static final int bad_bacon_joke=0x7f04000d;
         public static final int deliver_bacon=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int show_bacon_treats=0x7f040003;
     }
 }
