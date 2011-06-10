471734f40ed40f0715e47da5af57fa824449a49c
Mike Desjardins <mike.desjardins@cereslogic.com>
2011-06-10 11:01:12 -0400

    Added empty.xml.

--- gen/com/cereslogic/androidpreso/R.java
+++ gen/com/cereslogic/androidpreso/R.java
@@ -15,10 +15,12 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int bacon_image=0x7f050000;
+        public static final int bacon_image=0x7f050001;
+        public static final int empty_text_view=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int empty=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
